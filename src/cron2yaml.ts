59c0131169215461faedc003dbdc49468dc25cd5
Claudio Canales <ccanales@progrexion.com>
2019-02-19 11:36:53 -0700

    Working version

--- src/cron2yaml.ts
+++ src/cron2yaml.ts
@@ -21,47 +21,49 @@ export function activate(context: vscode.ExtensionContext) {
             }
 
             let yaml = "";
-            let text = doc.getText(cur_selection).split(/\r\n|\r|\n/);
+            let text = doc.getText(cur_selection).split(/\r\n|\r|\n/)
 
             for (let line of text) {
-                if (line.startsWith("#")) { // It's a comment
+                line = line.replace(/\s{2,}/g, ' '); // Cleaning double spaces
+                line = line.replace(/\s+$/, ''); // Remove trailing spaces
+                if (line.startsWith("#")) { // It's a comment entry
                     previous = line;
                 }
                 else {
                     let cron_values = line.split(" ");
                     if (cron_values.length < 6) {
-                        console.log("Invalid cron entry: " + line);
+                        //console.log(cron_values.length);
                         yaml += line + "\n";
                     }
                     else {
-                        yaml += "cronjob_:" + cron_values.slice(5).join("") + "\n" + "  " + "cron.present:" + "\n" + "    -name: " + cron_values.slice(5).join(" ") + "\n" + "    -user: root" + "\n";
+                        if (!previous.startsWith("#")) {
+                            yaml += "\n";
+                        }
+                        yaml += "cronjob_:" + cron_values.slice(5).join("") + ":" + "\n" + "  " + "cron.present:" + "\n" + "    - name: " + cron_values.slice(5).join(" ") + "\n" + "    - user: root" + "\n";
                         if (cron_values[0] !== "*") {
-                            yaml += "    -minute: " + cron_values[0] + "\n";
+                            yaml += "    - minute: " + cron_values[0] + "\n";
                         }
                         if (cron_values[1] !== "*") {
-                            yaml += "    -hour: " + cron_values[1] + "\n";
+                            yaml += "    - hour: " + cron_values[1] + "\n";
                         }
                         if (cron_values[2] !== "*") {
-                            yaml += "    -daymonth: " + cron_values[2] + "\n";
+                            yaml += "    - daymonth: " + cron_values[2] + "\n";
                         }
                         if (cron_values[3] !== "*") {
-                            yaml += "    -month: " + cron_values[3] + "\n";
+                            yaml += "    - month: " + cron_values[3] + "\n";
                         }
                         if (cron_values[4] !== "*") {
-                            yaml += "    -dayweek: " + cron_values[4] + "\n";
+                            yaml += "    - dayweek: " + cron_values[4] + "\n";
                         }
                         if (previous.startsWith("#")) {
-                            yaml += "    -comment:" + previous.replace('#', '') + "\n";
-                        }
-                        else {
-                            yaml += "\n";
+                            yaml += "    - comment:" + previous.replace('#', '') + "\n";
                         }
                     }
                 }
                 previous = line;
             }
-            editor.edit(edit => {
-                edit.replace(cur_selection, yaml);
+            editor.edit(edit => {// Replacing the text in the editor
+                edit.replace(cur_selection, yaml.replace(/(\n\s*?\n)\s*\n/, '$1')); //Removing multiple consecutive linebreaks
             });
         }
     });
